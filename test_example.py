05ed5545c2481a5851cc5c202f6a5f556cd3410a
Soul-Keeper12986 <ivashechkin2001@mail.ru>
2024-04-05 10:26:23 +0300

    Fixed_fixture
    Fixed_fixture

--- test_example.py
+++ test_example.py
@@ -1,13 +1,21 @@
 from time import sleep
+
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get('https://soreal.ru/uilyam-batler-jejts/')
 
-text_area = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Введите слова для поиска"]')
-link_next = driver.find_element(By.CSS_SELECTOR,"a[rel='next']")
-link_next.click()
-sleep(5)
-driver.quit()
+@pytest.fixture
+def driver(request):
+    driver = webdriver.Chrome()
+    print(driver.capabilities)
+    request.addfinalizer(driver.quit)
+    return driver
+
+def test_exaple1(driver):
+    driver.get('https://soreal.ru/uilyam-batler-jejts/')
+    link_next = driver.find_element(By.CSS_SELECTOR,"a[rel='next']")
+    link_next.click()
+    sleep(5)
+    driver.quit()
 
