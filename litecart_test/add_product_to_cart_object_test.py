1f836585ae828a3c516c2f375efc9811a2b2ab8b
Soul-Keeper12986 <ivashechkin2001@mail.ru>
2024-05-06 13:05:10 +0300

    try in OOP

---
+++ litecart_test/add_product_to_cart_object_test.py
@@ -0,0 +1,97 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class MainPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open(self):
+        self.driver.get("http://localhost/litecart/en/")
+
+    def select_first_product(self):
+        first_product = self.driver.find_element(By.CSS_SELECTOR, '#box-most-popular > div > ul > li:nth-child(1)')
+        first_product.click()
+
+
+class ProductPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def add_to_cart(self):
+        if self.driver.find_element(By.CSS_SELECTOR,
+                                    '#box-product > div:nth-child(1) > h1').text == 'Yellow Duck':
+            size = self.driver.find_element(By.NAME, 'options[Size]')
+            Select(size).select_by_visible_text("Small")
+        add_to_cart = self.driver.find_element(By.NAME, 'add_cart_product')
+        add_to_cart.click()
+
+
+class CartPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open(self):
+        cart = self.driver.find_element(By.CSS_SELECTOR, '#cart > a.content')
+        cart.click()
+
+    def remove_product(self):
+        remove_button = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.NAME, 'remove_cart_item')))
+        remove_button.click()
+        return remove_button  # Возвращаем кнопку после выполнения клика
+
+
+class TestShoppingFlow:
+    PRODUCT_COUNT = 3
+
+    def setup_method(self):
+        self.driver = webdriver.Chrome()
+        self.main_page = MainPage(self.driver)
+        self.product_page = ProductPage(self.driver)
+        self.cart_page = CartPage(self.driver)
+
+    def teardown_method(self):
+        if self.driver:
+            self.driver.quit()
+
+    def test_shopping_flow(self):
+        self.main_page.open()
+
+        cart_item_element = self.driver.find_element(By.CSS_SELECTOR, '#cart > a.content > span.quantity')
+        while int(cart_item_element.text) < self.PRODUCT_COUNT:
+            self.main_page.select_first_product()
+            self.product_page.add_to_cart()
+
+            original_cart_count = int(
+                self.driver.find_element(By.CSS_SELECTOR, '#cart > a.content > span.quantity').text)
+
+            WebDriverWait(self.driver, 10).until(
+                EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#cart > a.content > span.quantity"),
+                                                 str(original_cart_count + 1)))
+            self.driver.back()
+            cart_item_element = self.driver.find_element(By.CSS_SELECTOR, '#cart > a.content > span.quantity')
+
+        self.cart_page.open()
+
+        ul_element = self.driver.find_element(By.CSS_SELECTOR, "#box-checkout-cart > ul")
+        counter = int(len(ul_element.find_elements(By.TAG_NAME, "li")))
+        while counter != 0:
+            counter -= 1
+            remove_button = self.cart_page.remove_product()
+            WebDriverWait(self.driver, 10).until(EC.staleness_of(remove_button))  # Ждем исчезновения кнопки
+            if counter != 0:
+                WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, 'table.dataTable.rounded-corners')))
+            elif WebDriverWait(self.driver, 10).until(
+                    EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#checkout-cart-wrapper > p:nth-child(1) > em'),
+                                                     'There are no items in your cart.')):
+                self.driver.quit()
+
+
+if __name__ == "__main__":
+    pytest.main(args=['-s', __file__])
